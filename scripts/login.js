14601368fd926a9b21229e0c92d63b18bf3c1a57
macondotest <167193107+macondotest@users.noreply.github.com>
2024-06-19 07:33:45 -0500

    Create login.js

---
+++ scripts/login.js
@@ -0,0 +1,29 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCzgrvaRodfuiY1pF6dveXiLDmI0St64qc",
+    authDomain: "macondotest-904af.firebaseapp.com",
+    projectId: "macondotest-904af",
+    storageBucket: "macondotest-904af.appspot.com",
+    messagingSenderId: "853635517373",
+    appId: "1:853635517373:web:f5439d310e3e114ecba0d4"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Show specific view based on authentication status
+function showGame() {
+    window.location.href = "../gameplay/index.html";
+}
+
+// Login
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            showGame();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+});
