c986ba375adb50b011e5d27a73af907d3a3abad2
macondotest <167193107+macondotest@users.noreply.github.com>
2024-06-19 08:53:45 -0500

    Update gameplay.js

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -234,12 +234,14 @@ function drawCards(num) {
 }
 
 function getCardNumericValue(card) {
-        const cardValue = {
-            '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-            'jack': 12, 'queen': 13, 'king': 14, 'ace': 15
-        };
-        return cardValue[card.split('_')[0]];
-    }
+    const cardValue = {
+        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+        'jack': 12, 'queen': 13, 'king': 14, 'ace': 15
+    };
+    const cardName = card.split('/').pop().split('_')[0]; // Extract card name from the path
+    return cardValue[cardName];
+}
+
     function getCardSuit(card) {
         return card.split('_')[2].replace('.png', '');
     }
