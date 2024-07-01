3c0d75ab1a15b0e277466ae71941e56670f214fc
macondotest <167193107+macondotest@users.noreply.github.com>
2024-07-01 07:46:25 -0500

    Update gameplay.js

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -208,7 +208,11 @@ document.querySelectorAll('.option button').forEach(button => {
         if (wagerAmount > currentBalance) {
             alert('Insufficient balance.');
             return;
+            } else if ((currentBalance >= 500 && wagerAmount < 25) || (currentBalance >= 1000 && wagerAmount < 50)) {
+            alert('Insufficient wager amount.');
+            return;
         }
+        
         currentBalance -= wagerAmount;
         await updateBalanceInDB(currentBalance);
         await updateBalanceDisplay();
